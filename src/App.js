13ebaf09339b2a7d3bc8568260a4897378d70540
student 7 <unkown>
2024-03-10 18:53:40 +0300

    useEffect 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [yukleniyor, yukleniyorGuncelle] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://jsonplaceholder.org/users');
+      const jsonData = await response.json();
+      setData(jsonData);
+      yukleniyorGuncelle(false);
+    }
+    setTimeout(fetchData, 2000)
+  }, []);
   return (
       <>
-        App
+        {yukleniyor && <p>Yukleniyor... </p>}
+        {data.map(kisi => <p key={kisi.id}>{kisi.firstname}</p>)}
       </>
   );
 }
-
 export default App;
+
